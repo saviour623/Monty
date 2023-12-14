c96531d647fc51085a46e3fdab21d14bbac7b9a7
saviour623 <saviourme33@gmail.com>
2023-12-14 17:06:28 +0100

    new push

---
+++ 0_98_monty.c
@@ -0,0 +1,125 @@
+#include "monty.h"
+#include <stdbool.h>
+
+/**
+ * delimCharcmp - check if a char is a delimiter.
+ * @delim: delimiter
+ * @cmp: character
+ * Return: true if it a delimiter, else false
+ */
+int delimCharcmp(const char *restrict delim, const char *restrict cmp)
+{
+	char c __unused__;
+
+	if (delim == NULL || cmp == NULL)
+		return (-1);
+
+	if (delim[0] != 0 && delim[1] == 0)
+		return *delim == *cmp;
+
+	for (; (c = *delim); delim++)
+	{
+		if (c == *cmp)
+			return (true);
+	}
+	return (false);
+}
+int getNumtoks(const char *__restrict__ st, const char *__restrict__ delim)
+{
+	bool set;
+	register int oo;
+
+	if (st == NULL || delim == NULL)
+		return -1;
+
+	if (*st == '\0' || *delim == '\0')
+		return 0;
+
+	oo = 0;
+	set = true;
+
+	for (; *st; (void)*st++)
+	{
+		if (delimCharcmp(delim, st))
+			set = true;
+		else if (set)
+		{
+			oo++;
+			set = false;
+		}
+	}
+	return oo;
+}
+char **getcmdstring(char *__restrict__ arg)
+{
+	char *delim = " \t\r\n";
+	char **buftok;
+	char *token = NULL;
+	int oo;
+
+	if ((arg == NULL) || (*arg == '\0'))
+		return NULL;
+
+	buftok = malloc((sizeof(char *) * (getNumtoks(arg, delim))) + 1);
+
+	if (buftok == NULL)
+	{
+		fprintf(stderr, "ERROR: malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
+
+	token = arg;
+	for (oo = 0; (token = strtok(token, delim)) != NULL; oo++)
+	{
+		buftok[oo] = token;
+		token = NULL;
+	}
+
+	buftok[oo] = NULL;
+	return buftok;
+}
+
+int main(int argc, char **argv)
+{
+	register int fd;
+	FILE *F;
+	char *bytefile = NULL;
+	char *ln = NULL, *instrc = NULL;
+	size_t rd = 0;
+
+	if (argc != 2)
+	{
+		fprintf(stderr, "%s\n", "USAGE: monty file");
+		exit(EXIT_FAILURE);
+	}
+	bytefile = argv[argc - 1];
+	F = fopen(bytefile, "r");
+
+	if (F == NULL)
+	{
+		fprintf(stderr, "Error: Can't open file <%s>\n", bytefile);
+		exit(EXIT_FAILURE);
+	}
+	while (1)
+	{
+		if (getline(&ln, &rd, F) == -1)
+		{
+			if (errno == ENOMEM)
+			{
+				fprintf(stderr, "Error: Can't open file <%s>\n", bytefile);
+				exit(EXIT_FAILURE);
+			}
+			break;
+		}
+		instrc = getcmdstring(ln);
+		rd = 0;
+		free(ln);
+		ln = NULL;
+	}
+	if (fclose(F) == EOF)
+	{
+		perror("flcose");
+		exit(EXIT_FAILURE);
+	}
+	return 0;
+}
